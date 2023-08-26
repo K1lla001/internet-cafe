d75afd1517dba16078d3d2ecd9fc9edcdf1b21f9
rahmn <rrahman2298@gmail.com>
2023-08-26 20:33:37 +0700

    [IMPROVE] Payment by paypal

---
+++ src/main/java/com/atm/inet/service/PaypalService.java
@@ -0,0 +1,20 @@
+package com.atm.inet.service;
+
+import com.paypal.api.payments.Payment;
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.PayPalRESTException;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface PaypalService {
+    public Payment createPayment(
+            Double total,
+            String currency,
+            String method,
+            String intent,
+            String description,
+            String cancelUrl,
+            String successUrl
+    ) throws PayPalRESTException;
+    public Payment executePayment(String paymentId, String payerId) throws PayPalRESTException;
+}
